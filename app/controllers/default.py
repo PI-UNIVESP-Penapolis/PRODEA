cd4f57f5f7bdfbeb696e04c2ba74a575354a598f
Kenny Robert Gomes Da Silva <2006315@aluno.univesp.br>
2021-10-13 19:40:44 -0300

    paginas html

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -2,14 +2,18 @@ from flask import template_rendered
 from flask.templating import render_template
 from app import app
 
-@app.route("/")
-def route():
-    return "teste"
+from  app.models.forms import CasdastroForm
 
+@app.route("/")
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('hometeste.html')
 
 @app.route('/entidades')
 def entidades():
-    return render_template('entidades')
+    return render_template('entidades.html')
+
+@app.route('/cadastre')
+def cadastre():
+    form = CasdastroForm()
+    return render_template('cadastro.html', form=form)
---
+++ app/models/forms.py
@@ -0,0 +1,14 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, TextField
+import wtforms
+from wtforms.validators import DataRequired
+
+
+class CasdastroForm(FlaskForm):
+    nome = StringField("nome", validators=[DataRequired()])
+    cnpj = StringField("cnpj", validators=[DataRequired()])
+    rua = StringField("rua", validators=[DataRequired()])
+    bairro = StringField("bairro", validators=[DataRequired()])
+    cidade = StringField("cidade", validators=[DataRequired()])
+    cep = StringField("cep", validators=[DataRequired()])
+    sobre = TextField("sobre", validators=[DataRequired()])
