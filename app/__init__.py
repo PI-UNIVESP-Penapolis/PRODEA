29d1532c9cdb54d1370d2fa3618157175bc664b4
Kenny Robert Gomes Da Silva <2006315@aluno.univesp.br>
2021-10-14 21:57:53 -0300

    requisitos Heroku

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,22 @@
+import os
 from flask import Flask
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
+
 
 from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object('config')
+
+cors = CORS(app, resource={r"/*":{"origins": "*"}})
+
+def main():
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+if __name__ =+ "__main__"
+    main()
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
