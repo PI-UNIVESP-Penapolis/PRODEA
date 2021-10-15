cd11b32dd9c3c3e6f1013febbcc1d32c3bf09b49
Kenny Robert Gomes Da Silva <2006315@aluno.univesp.br>
2021-10-14 22:15:52 -0300

    mais modificações Heroku

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,5 @@
-import os
+
 from flask import Flask
-from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -9,14 +8,8 @@ from flask_migrate import Migrate
 app = Flask(__name__)
 app.config.from_object('config')
 
-cors = CORS(app, resource={r"/*":{"origins": "*"}})
-
-def main():
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
-
-if __name__ =+ "__main__"
-    main()
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
