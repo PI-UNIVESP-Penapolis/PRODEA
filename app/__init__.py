5a0278f9f6e49e27df266a7d72c246f94b47a812
Kenny Robert Gomes Da Silva <2006315@aluno.univesp.br>
2021-11-24 21:47:11 -0300

    tentando corrigir lerdeza

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
@@ -9,6 +10,7 @@ app = Flask(__name__)
 app.config.from_object('config')
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0')
 
 db = SQLAlchemy(app)
