71438c145c6b96b16933881ebfd09a57496265f2
Kenny Robert Gomes Da Silva <2006315@aluno.univesp.br>
2021-11-27 22:32:51 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,14 @@
+import django_heroku
+
 DEBUG = True
 import os.path
 
 PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
-
+ALLOWED_HOSTS = ["https://prodea.herokuapp.com/"]
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(PROJECT_ROOT, 'app', 'storage.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 SECRET_KEY = 'um-nome-bem-seguro'
+
+django_heroku.settings(locals())
